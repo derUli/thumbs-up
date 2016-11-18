affdabe48f035fc91ccde7955269b33752c27834
Ulrich Schmidt <u.schmidt@velian.de>
2016-11-18 15:08:33 +0100

    javascript korrigiert

--- src/content/modules/thumbs_up/js/thumbs.js
+++ src/content/modules/thumbs_up/js/thumbs.js
@@ -3,14 +3,39 @@ function updateThumbs(element){
    var dat = localStorage.getItem("thumb_for_" + id);
    var imgdir = $("#image-dir").val();
    if(dat == "up"){
-     $(".thumbs-up img").first().attr("src", imgdir + "/up-voted.png");
+       $(".thumbs-up img").first().attr("src", imgdir + "/up-voted.png");
        $(".thumbs-down img").first().attr("src", imgdir + "/down.png");
      } else if(dat == "down"){
          $(".thumbs-up img").first().attr("src", imgdir + "/up.png");
-           $(".thumbs-down img").first().attr("src", imgdir + "/down-voted.png");
+         $(".thumbs-down img").first().attr("src", imgdir + "/down-voted.png");
    }
+}
+
+function thumbUp(element){
+     var id = $(element).data("id");
+     var dat = localStorage.getItem("thumb_for_" + id);
+     if(dat == null){
+       localStorage.setItem("thumb_for_" + id, "up");
+       updateThumbs(".thumbs-container");
+     }
+}
+
+function thumbDown(element){
+     var id = $(element).data("id");
+     var dat = localStorage.getItem("thumb_for_" + id);
+     if(dat == null){
+       localStorage.setItem("thumb_for_" + id, "down");
+       updateThumbs(".thumbs-container");
+     }
 }
 
 $(document).ready(function(){
    updateThumbs(".thumbs-container");
+   $("a.thumbs-up").click(function(){
+       thumbUp(this);
+   });
+
+   $("a.thumbs-down").click(function(){
+       thumbDown(this);
+   });
 })
