f132c73023203efc6c495af84e64905ce5c6baf3
Ulrich Schmidt <u.schmidt@velian.de>
2016-11-18 15:56:30 +0100

    translation

--- src/content/modules/thumbs_up/lang/en.php
+++ src/content/modules/thumbs_up/lang/en.php
@@ -1,3 +1,4 @@
 <?php
 define("TRANSLATION_VOTE_UP", "Vote Up");
 define("TRANSLATION_VOTE_DOWN", "Vote Down");
+define("TRANSLATION_ONLY_REGISTERED_USERS_CAN_VOTE", "Only registered users can vote");
