11c42c0634efba8ff0a08f31a014b40bc24df107
Ulrich Schmidt <u.schmidt@velian.de>
2016-11-18 13:39:43 +0100

    syntax fehler korrigiert

--- src/content/modules/thumbs_up/templates/rating.php
+++ src/content/modules/thumbs_up/templates/rating.php
@@ -13,16 +13,24 @@ if(Settings::get("thumbs_up_only_registered_users_can_vote")){
   }
 }
 
+  $upImage = $imageDir . "/up";
+  $downImage = $imageDir . "/down";
+if(!$canRate){
+  $upImage .= "-disabled";
+  $downImage .= "-disabled";
+}
+  $upImage .= ".png";
+  $downImage .= ".png";
 ?>
-<input type="hidden" id="image-dir"  value="<?php echo $imageDir;">
+<input type="hidden" id="image-dir"  value="<?php echo $imageDir;?>">
 <div class="thumbs-container">
   <div class="thumbs-button">
-    <?php if($canRate){?><a href="#" class="thumbs-up"><?php }?><img src="<?php echo $imageDir;?>/up.png" alt="<?php translate("vote_up");?>" title="<?php translate("vote_up");?>"><?php if($canRate){?></a><?php }?>
+    <?php if($canRate){?><a href="#" class="thumbs-up"><?php }?><img src="<?php echo $upImage;?>" alt="<?php translate("vote_up");?>" title="<?php translate("vote_up");?>"><?php if($canRate){?></a><?php }?>
 <p><?php echo $page["thumbs_up"];?></p>
   </div>
   <div class="thumbs-button">
 
-      <?php if($canRate){?><a href="#" class="thumbs-down"><?php }?><img src="<?php echo $imageDir;?>/down.png" alt="<?php translate("vote_down");?>" title="<?php translate("vote_down");?>">
+      <?php if($canRate){?><a href="#" class="thumbs-down"><?php }?><img src="<?php echo $downImage;?>" alt="<?php translate("vote_down");?>" title="<?php translate("vote_down");?>">
         <?php if($canRate){?></a><?php }?>
 <p><?php echo $page["thumbs_down"];?></p>
   </div>
