a78868b2e7b87bcf30da0edb637cbb2bb30b291a
Ulrich Schmidt <u.schmidt@velian.de>
2016-11-18 10:23:50 +0100

    API geschaffen

---
+++ src/content/modules/thumbs_up/hooks/before_http_header.php
@@ -0,0 +1,21 @@
+<?php
+
+$canRate = true;
+
+if(Settings::get("thumbs_up_only_registered_users_can_vote") and !is_logged_in()){
+  $canRate = false;
+}
+
+if($canRate){
+    if(isset($_POST["vote_up"])){
+        $id = intval($_POST["vote_up"]);
+        $args = array($id);
+        $sql = "UPDATE {prefix}content set thumbs_up = thumbs_up + 1 where id = ?";
+        Database::pQuery($sql, $args, true);
+    } else if(isset($_POST["vote_down"])){
+        $id = intval($_POST["vote_down"]);
+        $args = array($id);
+        $sql = "UPDATE {prefix}content set thumbs_down = thumbs_down + 1 where id = ?";
+        Database::pQuery($sql, $args, true);
+    }
+}
