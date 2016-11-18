b5669aafbc130d81b91e933e717f79c9d55ed8ee
Ulrich Schmidt <ulrich1992@web.de>
2016-11-18 19:43:51 +0100

    abstimmen

--- src/content/modules/thumbs_up/lang/de.php
+++ src/content/modules/thumbs_up/lang/de.php
@@ -1,4 +1,5 @@
 <?php
 define ( "TRANSLATION_VOTE_UP", "Daumen hoch" );
 define ( "TRANSLATION_VOTE_DOWN", "Daumen runter" );
-define ( "TRANSLATION_ONLY_REGISTERED_USERS_CAN_VOTE", "Nur eingeloggte Besucher können Abstimmen" );
+define ( "TRANSLATION_ONLY_REGISTERED_USERS_CAN_VOTE", "Nur eingeloggte Besucher können abstimmen" );
+define ( "TRANSLATION_THUMBS_UP", "Daumen hoch" );
